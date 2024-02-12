4c81bc6ed4749df5639710ba7a556a3a9fd16d41
Sgottabe <sgottabe@gmail.com>
2024-02-11 18:15:17 -0700

    added completed checkmark

--- src/components/Assignment/index.tsx
+++ src/components/Assignment/index.tsx
@@ -1,7 +1,9 @@
 // src/components/Assignment/index.tsx
 import styles from "./assignment.module.css";
-import React from 'react';
+import React, { useState } from 'react';
+import { BsCheck } from 'react-icons/bs';
 import { TbTrash } from 'react-icons/tb';
+
 
 interface AssignmentProps {
   title: string;
@@ -14,9 +16,9 @@ export function Assignment({ title, completed, onDelete, onToggleComplete }: Ass
   return (
     <div className={styles.assignment}>
       <button className={styles.checkContainer} onClick={onToggleComplete}>
-        <div />
+        {completed ? <BsCheck size={20} /> : <div />}
       </button>
-      <p>{title}</p>
+      <p className={completed ? styles.textCompleted : ''}>{title}</p>
       <button className={styles.deleteButton} onClick={onDelete}>
         <TbTrash size={20} />
       </button>
